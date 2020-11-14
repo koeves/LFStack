4424cb87da653094f95e5a247a2b063fc4c6cacb
Daniel Köves <58272685+koeves@users.noreply.github.com>
2020-11-14 15:03:13 +0100

    Add files via upload

--- LFStack.hpp
+++ LFStack.hpp
@@ -3,15 +3,13 @@
 #include <iostream>
 #include "Node.hpp"
 
-template <class T> 
-class LFStack {
+template <class T> class LFStack {
     
 public:
     LFStack() : top(nullptr) {}
     ~LFStack() {
         Node<T> *node = top;
         while (node) {
-            std::cout << "Destroying node " << node->value << std::endl;
             Node<T> *old = node;
             node = node->next;
             delete old;
@@ -24,7 +22,19 @@ public:
             else sleep(random() % 5);
         }
     }
-    void pop() {}
+    T pop() {
+        while (true) {
+            Node<T> *return_node = try_pop();
+            if (return_node) return return_node->value;
+            else sleep(random() % 5);
+        }
+    }
+
+    struct EmptyException : public std::exception {
+        const char *what() const throw() {
+            return "Stack is empty";
+        }
+    };
 
 protected:
     bool try_push(Node<T> *node) {
@@ -32,9 +42,19 @@ protected:
         node->next = old_top;
         return std::atomic_compare_exchange_strong(&top, &old_top, node);
     }
-    bool try_pop();
+    Node<T> *try_pop() {
+        try {
+            Node<T> *old_top = std::atomic_load(&top);
+            if (!old_top) throw EmptyException();
+            Node<T> *new_top = old_top->next;
+            return (std::atomic_compare_exchange_strong(&top, &old_top, new_top)) ? old_top : nullptr;
+        }
+        catch(EmptyException &e) {
+            std::cerr << e.what() << std::endl;
+            return nullptr;
+        }
+    }
 
 private:
     std::atomic<Node<T> *> top;
-
 };
